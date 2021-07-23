7aa96f5b4cd1c8ef2c76a76e836f0e6b4b200aa8
Stereowalker <35645830+Stereowalker@users.noreply.github.com>
2021-07-23 01:50:12 +0100

    Made minor changes to the paper doll screen code

--- src/main/java/com/stereowalker/controllermod/client/gui/screen/PaperDollOptionsScreen.java
+++ src/main/java/com/stereowalker/controllermod/client/gui/screen/PaperDollOptionsScreen.java
@@ -30,34 +30,38 @@ public class PaperDollOptionsScreen extends Screen {
 
 	@Override
 	public void init() {
-		addOption(true, 0, Config.isAttacking);
-		addOption(false, 0, Config.isBurning);
-		addOption(true, 1, settings.paperDoll.showCrawling, (get) -> settings.paperDoll.showCrawling = get, new TranslationTextComponent("gui.paper_doll.crawling"));
-		addOption(false, 1, settings.paperDoll.showCrouching, (get) -> settings.paperDoll.showCrouching = get, new TranslationTextComponent("gui.paper_doll.crouching"));
-		addOption(true, 2, Config.isElytraFlying);
-		addOption(false, 2, settings.paperDoll.showFlying, (get) -> settings.paperDoll.showFlying = get, new TranslationTextComponent("gui.paper_doll.flying"));
-		addOption(true, 3, Config.isHurt);
-		addOption(false, 3, Config.isJumping);
-		addOption(true, 4, Config.isMoving);
-		addOption(false, 4, Config.isRiding);
-		addOption(true, 5, Config.isSpinning);
-		addOption(false, 5, settings.paperDoll.showSprinting, (get) -> settings.paperDoll.showSprinting= get, new TranslationTextComponent("gui.paper_doll.sprinting"));
-		addOption(true, 6, settings.paperDoll.showSwimming, (get) -> settings.paperDoll.showSwimming = get, new TranslationTextComponent("gui.paper_doll.swimming"));
-		addOption(false, 6, Config.isUsing);
+		addOption( 0, Config.isAttacking);
+		addOption( 1, Config.isBurning);
+		addOption( 2, settings.paperDoll.showCrawling, (get) -> settings.paperDoll.showCrawling = get, new TranslationTextComponent("gui.paper_doll.crawling"));
+		addOption( 3, settings.paperDoll.showCrouching, (get) -> settings.paperDoll.showCrouching = get, new TranslationTextComponent("gui.paper_doll.crouching"));
+		addOption( 4, Config.isElytraFlying);
+		addOption( 5, settings.paperDoll.showFlying, (get) -> settings.paperDoll.showFlying = get, new TranslationTextComponent("gui.paper_doll.flying"));
+		addOption( 6, Config.isHurt);
+		addOption( 7, Config.isJumping);
+		addOption( 8, Config.isMoving);
+		addOption( 9, Config.isRiding);
+		addOption(10, Config.isSpinning);
+		addOption(11, settings.paperDoll.showSprinting, (get) -> settings.paperDoll.showSprinting= get, new TranslationTextComponent("gui.paper_doll.sprinting"));
+		addOption(12, settings.paperDoll.showSwimming, (get) -> settings.paperDoll.showSwimming = get, new TranslationTextComponent("gui.paper_doll.swimming"));
+		addOption(13, Config.isUsing);
 		this.addButton(new Button(this.width / 2 - 100, this.height  / 6 + 168, 200, 20, new TranslationTextComponent("gui.done"), (p_212984_1_) -> {
 			this.minecraft.displayGuiScreen(this.previousScreen);
 		}));
 	}
 	
-	public void addOption(boolean left, int index, BooleanValue value) {
-		this.addButton(new Button(this.width / 2 +(left?-155:5), this.height  / 6 + (24*index), 150, 20, new TranslationTextComponent(value.getPath().get(1)).appendString(" : ").appendSibling(DialogTexts.optionsEnabled(value.get())), (p_212984_1_) -> {
+	public void addOption(int index, BooleanValue value) {
+		boolean left = index%2 == 0;
+		int position = index / 2;
+		this.addButton(new Button(this.width / 2 +(left?-155:5), this.height  / 6 + (24*position), 150, 20, new TranslationTextComponent(value.getPath().get(1)).appendString(" : ").appendSibling(DialogTexts.optionsEnabled(value.get())), (p_212984_1_) -> {
 			value.set(!value.get());
 			this.minecraft.displayGuiScreen(new PaperDollOptionsScreen(previousScreen));
 		}));
 	}
 	
-	public void addOption(boolean left, int index, boolean getter, Consumer<Boolean> setter, IFormattableTextComponent text) {
-		this.addButton(new Button(this.width / 2 +(left?-155:5), this.height  / 6 + (24*index), 150, 20, text.appendString(" : ").appendSibling(DialogTexts.optionsEnabled(getter)), (p_212984_1_) -> {
+	public void addOption(int index, boolean getter, Consumer<Boolean> setter, IFormattableTextComponent text) {
+		boolean left = index%2 == 0;
+		int position = index / 2;
+		this.addButton(new Button(this.width / 2 +(left?-155:5), this.height  / 6 + (24*position), 150, 20, text.appendString(" : ").appendSibling(DialogTexts.optionsEnabled(getter)), (p_212984_1_) -> {
 			setter.accept(!getter);
 			this.minecraft.displayGuiScreen(new PaperDollOptionsScreen(previousScreen));
 		}));
