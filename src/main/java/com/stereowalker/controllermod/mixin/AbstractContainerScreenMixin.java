042efe447c96d9794c389133df59ec46da8b00ce
Stereowalker <35645830+Stereowalker@users.noreply.github.com>
2021-10-01 09:05:56 +0100

    Added changelog file

--- src/main/java/com/stereowalker/controllermod/mixin/AbstractContainerScreenMixin.java
+++ src/main/java/com/stereowalker/controllermod/mixin/AbstractContainerScreenMixin.java
@@ -24,20 +24,13 @@ public abstract class AbstractContainerScreenMixin <T extends AbstractContainerM
 		super(pTitle);
 	}
 	
-	/**
-	 * @owner Stereowalker
-	 */
 	@Redirect(method = {"mouseReleased", "mouseClicked"}, at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/InputConstants;isKeyDown(JI)Z"))
 	public boolean shiftClick(long l, int i) {
 		return Screen.hasShiftDown() || ControllerBindings.SHIFT_MOVE_INPUT.isDown(Config.controllerModel.get());
 	}
 	
-	/**
-	 * @owner Stereowalker
-	 */
-	@Redirect(method = "mouseReleased", at = @At(value = "INVOKE", target = "hasShiftDown()Z"))
+	@Redirect(method = "mouseReleased", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/inventory/AbstractContainerScreen;hasShiftDown()Z"))
 	public boolean shiftClick() {
 		return Screen.hasShiftDown() || ControllerBindings.SHIFT_MOVE_INPUT.isDown(Config.controllerModel.get());
 	}
-
 }
