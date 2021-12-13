7448e5f55c3de4fc30911501db5b221a4075518d
Stereowalker <35645830+Stereowalker@users.noreply.github.com>
2021-12-13 12:03:07 +0100

    Tweak gradle

--- changelog.md
+++ changelog.md
@@ -1,8 +1,6 @@
 [5.0.1]
-
-- Fixed some keybinds not registering. Now all registered keybinds from any mod should be bindable to a controller button
+- Fixed some keymaps not registering. Now all registered keymaps from any mod should be able to be bound to a controller button
 
 [5.0.0]
-
 - Initial update to 1.17.1
 - Shifted from using forge events to using mixins
--- src/main/java/com/stereowalker/controllermod/ControllerMod.java
+++ src/main/java/com/stereowalker/controllermod/ControllerMod.java
@@ -23,7 +23,6 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
